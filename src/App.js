9ef02a2e6fbb29282c3035edbc6c4ce197e2c6ec
Rajib Ghosh <rajib.ghosh@codeclouds.com>
2023-01-03 10:04:48 +0530

    add project with redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { lazy, Suspense } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import ContactUs from './screens/ContactUs';
+import Home from './screens/Home';
+import store from './store'
+
+const Shop = lazy(() => import('./screens/Shop'))
+const Cart = lazy(() => import('./screens/Cart'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path='/' exact element={<Home />} />
+              <Route path='/shop' element={<Shop />} />
+              <Route path='/contact-us' element={<ContactUs />} />
+              <Route path='/cart' element={<Cart />} />
+            </Routes>
+          </Suspense>
+        </Layout >
+      </BrowserRouter>
+    </Provider>
   );
 }
 
